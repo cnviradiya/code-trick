d8ff9892161f1a6b33db466abb3ed1bb7b0ec2db
Chirag Viradiya <viradiyachirag91@gmail.com>
2022-12-27 19:22:39 +0530

    Added trick to download the prevented google drive file

---
+++ download-google-drive-pdf/README.md
@@ -0,0 +1,8 @@
+## Steps to use this technique
+
+1. Open pdf file through drive link
+2. Scroll to the bottom of the pdf which you want to download
+3. Open Inpect element through Three dot of the google chrome
+4. Go to console window
+5. Execute the code which is in the file `download-drive-prevented-pdf.js`
+6. Withing few seconds you can see the download directory there are pdf file there
---
+++ download-google-drive-pdf/download-drive-prevented-pdf.js
@@ -0,0 +1,36 @@
+(function () {
+  const downloadFile = () => {
+    let jspdfEle = document.createElement('script');
+
+    jspdfEle.onload = function () {
+      let pdf = new jsPDF();
+      let elements = document.getElementsByTagName('img');
+      for (let i in elements) {
+        let img = elements[i];
+        if (!/^blob:/.test(img.src)) {
+          console.log('invalid src');
+          continue;
+        }
+        let can = document.createElement('canvas');
+        let con = can.getContext('2d');
+        can.width = img.width;
+        can.height = img.height;
+        con.drawImage(img, 0, 0, img.width, img.height);
+        let imgData = can.toDataURL('image/jpeg', 1.0);
+        pdf.addImage(imgData, 'JPEG', 0, 0);
+        pdf.addPage();
+      }
+  
+      pdf.save('ITCoders-' + new Date().getTime() + '-decoded-file.pdf');
+    };
+  
+    jspdfEle.src =
+      'https' +
+      '://' +
+      'cdnjs' +
+      '.cloudflare' +
+      '.com/ajax/libs/jspdf/1.5.3/jspdf.debug.js';
+    document.body.appendChild(jspdfEle);
+  }
+  downloadFile();
+})();
